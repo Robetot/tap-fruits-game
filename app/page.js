b25abbe00b856717851416564bf69a30d2576604
Robetot <121406852+Robetot@users.noreply.github.com>
2025-12-02 16:34:11 -0500

    app/page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "24px"
+    }}>
+      🍎 Tap Fruits Game
+    </div>
+  );
+}
